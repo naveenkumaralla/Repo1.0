fd06c78f542c57c8acbcfcf4d5a2b7a2597fb2f0
nani <nanialla15@gmail.com>
2026-08-26 12:03:41 +0530

    dev test branch

--- kvm-manager/lib/console.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-console_command() {
-    local vm=${1:-}
-    require_vm "$vm"
-
-    [[ "$(vm_state "$vm")" == "running" ]] || die "VM '$vm' must be running for console access."
-    virsh console "$vm"
-}
